dc768af1dfbb91bc63c2c24c5224159e5b756ff9
Renato Porto <renatoafporto@gmail.com>
2020-05-06 21:11:02 -0300

    Text captured from image is parsed to object

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "./config/.env") });
 // const Jimp = require("jimp");
 // const QRCode = require("qrcode-reader");
 // const express = require("express");
-const TelegramBot = require("node-telegram-bot-api");
+// const TelegramBot = require("node-telegram-bot-api");
 const Vision = require("@google-cloud/vision");
 
 // const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
@@ -38,16 +38,31 @@ const Vision = require("@google-cloud/vision");
 //   }
 // });
 
-const imgToText = async (fileURI) => {
+const imgToText = async (fileName) => {
   /* GOOGLE VISION API */
   const client = new Vision.ImageAnnotatorClient();
 
-  const [result] = await client.documentTextDetection(fileURI);
+  const [result] = await client.textDetection(fileName);
   const detections = result.textAnnotations[0];
-  console.log(detections.description);
+
+  var array = detections.description.split(/\r?\n/)
+  array = array.filter(a => a.length > 13)
+  array = array.map(product => {
+    return product = {
+      name: product.substring(13, 28).trim(),
+      quantity: parseInt(product.substring(29, 34).trim().replace(',', '.')),
+      price: parseFloat(product.slice(-5).trim().replace(',', '.'))
+    }
+  })
+
+  const total = array.reduce((acc, curr) => {
+    return curr.price + acc
+  }, 0)
+  console.log(array);
+  console.log(`Total: ${total.toFixed(2)}`)
 }
 
-imgToText('./img/sample4.jpg')
+imgToText('./img/sample-crop.jpg')
 
 /* EXPRESS SERVER */
 // const app = express();
